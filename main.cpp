7b9aff91fd5919e27a74d231e9ef9537ec45a74a
saidahshaik <saida9000shaik@gmail.com>
2022-08-13 08:54:08 +0530

    sorted In desc

--- main.cpp
+++ main.cpp
@@ -6,12 +6,18 @@
 using namespace std;
 int main()
 {
- vector<int>v1={10,20,30,40};
-
+ vector<int>v1={10, 20, 30, 40};
+ int sum = 0;
  cout<<"v1:{";
- for_each(begin(v1),end(v1),[](const auto& x){cout<<x<<"";});
-  cout<<"}";
+ for_each(begin(v1),end(v1),[&sum](const auto& x){cout<<x<<""; sum += x;});
+  cout<<"}"<<endl;
 
+  cout<<"sum of vector elements"<< sum <<endl;
   cout<<endl;
+  //sort elements in decending order
+  sort(v1.begin(),v1.end(),greater<int>());
+  cout<<"v1:{";
+  for_each(begin(v1),end(v1),[](const auto& x){cout<<x<<" ";});
+  cout<<"}"<<endl;
   return 0;
 } 
